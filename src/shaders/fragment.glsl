51fdf888f10729adbbb7f75852c4c17e927a069e
Hung Chun Fai <arkon.digital.hcf@gmail.com>
2023-09-16 04:25:11 +0900

    adding more code comments, refactored atmospheric coloring calculation to use dot(eyeVector, vNormal) as starting point for easier understanding

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -1,16 +1,19 @@
 // reference from https://github.com/dataarts/webgl-globe/blob/8d746a3dbf95e57ec3c6c2c6effe920c95135253/globe/globe.js
 varying vec3 vNormal;
+varying vec3 eyeVector;
+uniform float atmOpacity;
+uniform float atmPowFactor;
+uniform float atmMultiplier;
+
 void main() {
-    // the dot product would decrease from 0 to negative one
-    // thus making the base gradually larger towards the sphere center
-    // the larger the power the quicker the change
-    // if we don't switch to backside and use the default frontside instead
-    // the dot product would increase from 0 to positive one
-    // and thus making the base even smaller as it goes towards the sphere center
-    float dotP = dot( vNormal, vec3( 0, 0, 1.0 ) );
-    float intensity = pow( 0.8 - dotP, 4.5 );
-    // also add change in dotP to the color to make it brighter and less blue towards the center
-    gl_FragColor = vec4( 0.4 - dotP/2.0, 0.4 - dotP/2.0, 1.0, 0.65 ) * intensity;
+    // Starting from the atmosphere edge, dotP would increase from 0 to 1
+    float dotP = dot( vNormal, eyeVector );
+    // This factor is to create the effect of a realistic thickening of the atmosphere coloring
+    float factor = pow(dotP, atmPowFactor) * atmMultiplier;
+    // Adding in a bit of dotP to the color to make it whiter while thickening
+    vec3 atmColor = vec3(0.35 + dotP/4.5, 0.35 + dotP/4.5, 1.0);
+    // use atmOpacity to control the overall intensity of the atmospheric color
+    gl_FragColor = vec4(atmColor, atmOpacity) * factor;
 
     // (optional) colorSpace conversion for output
     // gl_FragColor = linearToOutputTexel( gl_FragColor );
